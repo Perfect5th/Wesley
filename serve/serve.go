3e2d3bd1defe98e1dd52b1b3b488a39f41520c2c
Mitchell Burton <mitch.b45@gmail.com>
2017-12-03 17:20:19 -0800

    generic commit message

--- serve/serve.go
+++ serve/serve.go
@@ -1,7 +1,26 @@
 package serve
 
-import "net/http"
+import (
+  "fmt"
+  "io"
+  "io/ioutil"
+  "log"
+  "net/http"
+  "os"
+)
+
+func ShowIndex(w http.ResponseWriter, req *http.Request) {
+  base_dir, _ := os.Getwd()
+  files, _ := ioutil.ReadDir(base_dir)
+
+  log.Printf("%s %s", req.Method, req.URL.Path)
+  for i := range files {
+    nameStr := fmt.Sprintf("%s\n", files[i].Name())
+    io.WriteString(w, nameStr)
+  }
+}
 
 func Start(port string) {
-  http.ListenAndServe(port, nil)
+  http.HandleFunc("/", ShowIndex)
+  log.Fatal(http.ListenAndServe(port, nil))
 }
